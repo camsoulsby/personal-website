1c8582d0fa74ffd1910a20ecc3eebc786b1f9060
Cam <cameron.soulsby@gmail.com>
2023-01-06 08:25:31 +1300

    Intial commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+   
+      <h1>Cam Soulsby</h1>
+    
+        <p>
+          Operations | Software Development
+        </p>
+      
+     
+    </div>
+  )
+}
+
+export default App
