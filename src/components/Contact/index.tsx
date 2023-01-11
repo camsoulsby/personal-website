f887e0387cd7bd99294170a37e71b65d131cb9d7
Cam <cameron.soulsby@gmail.com>
2023-01-12 08:35:45 +1300

    complete contact section

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -1,13 +1,45 @@
-import React from "react";
-import { Typography, Container, Button, Grid } from "@mui/material";
+import React, { useState } from "react";
+import { Typography, Container, Button, TextField, Grid } from "@mui/material";
 
 export const Contact: React.FunctionComponent = () => {
+  const [emailText, setEmailText] = useState('');
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEmailText(event.target.value);
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    window.open(`mailto:cameron.soulsby+webform@gmail.com?subject=Web Enquiry&body=${emailText}`);
+
+  };
   return (
     <div>
         <Container maxWidth={false} sx={{backgroundColor:'secondary.main', color: 'primary.main'}}>
-        <Container sx={{ paddingTop: "150px", paddingBottom: "200px" }}>
+        <Container sx={{ paddingTop: "100px", paddingBottom: "150px" }}>
         <Typography variant="h2" align='center'>Contact Me</Typography>
-     <p>email form here</p>
+        
+        <form noValidate autoComplete="off" onSubmit={handleSubmit}>
+        <Grid container flexDirection={'column'}>
+
+        <Grid item sx={{textAlign: 'center', paddingTop: '10px'}}>
+      <TextField sx={{width: '800px'}}
+        id="standard-multiline-static"
+        multiline
+        rows={12}
+        placeholder="Enter message"
+        margin="normal"
+        onChange={handleChange}
+      />
+      </Grid>
+      <Grid item sx={{textAlign: 'center', paddingTop: '30px'}}>
+      <Button variant="contained" color="primary" type="submit">
+        Send Email
+      </Button>
+      </Grid>
+      </Grid>
+    </form>
+    
         </Container>
       </Container>
     </div>
