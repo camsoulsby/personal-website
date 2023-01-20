a666473fa313cb842114f80540ded40a85c4697f
Cam <cameron.soulsby@gmail.com>
2023-01-20 17:42:07 +1300

    create collapsable table layout

--- src/components/Experience/index.tsx
+++ src/components/Experience/index.tsx
@@ -1,197 +1,182 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Typography,
   Container,
   Table,
-  TableHead,
   TableRow,
   TableCell,
   TableBody,
+  Box,
 } from "@mui/material";
 
 export const Experience: React.FunctionComponent = () => {
-  const tableHeadStyle1 = {
-    color: "secondary.main",
-    fontWeight: "700",
-    width: "200px",
-  };
-  const tableHeadStyle2 = {
-    color: "secondary.main",
-    fontWeight: "700",
-    width: "200px",
-  };
-  const tableHeadStyle3 = {
-    color: "secondary.main",
-    fontWeight: "700",
-    width: "110px",
-  };
-  const tableHeadStyle4 = { color: "secondary.main", fontWeight: "700" };
+  const [tableRowsShown, setTableRowsShown] = useState([false, false]);
 
-  const tableBodyStyle = {
-    backgroundColor: "secondary.main",
-    border: "1px solid",
-    borderColor: "primary.main",
+  const toggleRowShown = (index: number) => {
+    console.log(`toggling row ${index}`);
+    const newRowsShown = [...tableRowsShown];
+    newRowsShown[index] = !newRowsShown[index];
+    setTableRowsShown(newRowsShown);
+  };
+
+  function createData(
+    year: string,
+    company: string,
+    title: string,
+    location: string,
+    description: string[]
+  ) {
+    return { year, company, title, location, description };
+  }
+  // add data
+  const rows = [
+    createData(
+      "2021-2022",
+      "mStable",
+      "Head of Operations",
+      "Remote",
+      ["Leading operations for a team of 15 building a Decentralised Finance application",
+       "Handling day to day HR, payroll, accounting, and legal tasks for legal entities in Germany, Australia and New Zealand as well as for a Decentralized Autonomous Organization",
+      "Navigating a complex legal and regulatory environment in an emerging space",
+      "Working with a global and fully remote team and managing the resulting coordination challenges"
+    ]
+    ),
+    createData(
+      "2020-2021",
+      "NZSki",
+      "Guest Services Head of Department",
+      "Queenstown, New Zealand",
+      ["Leading a team of frontline Guest Services staff as well as a Reservations team, responding to a demanding flow of incoming calls and emails",
+       "Working closely with other NZSki teams to improve products and systems "]
+    ),
+    createData(
+      "2015-2019",
+      "NZSki",
+      "Lift Operations Head of Department",
+      "Queenstown, New Zealand",
+      ["Managing a team of up to 45 Lift Operators and 3 Team Leaders",
+       "Recruiting staff, organising and co-ordinating training, assessing qualifications, departmental planning, developing manuals and procedures, creating rosters, purchasing, performance reviews, attending operations meetings and working closely with other departments",
+        "Overseeing daily operations to ensure exceptional customer service"]
+    ),
+    createData(
+      "2016-2017",
+      "Niseko Village",
+      "Lift Operations Team Leader",
+      "Hokkaido, Japan",
+      ["Part of Niseko Village’s first-ever group of native English-speaking Lift Operators"]
+    ),
+    createData(
+      "2014",
+      "NZSki",
+      "Lift Operations Team Leader",
+      "Queenstown, New Zealand",
+      ["Helping to lead a team of 35 Lift Operators"]
+    ),
+    createData(
+      "2012-2013",
+      "NZSki",
+      "Lift Operator",
+      "Queenstown, New Zealand",
+      ["Working as part of the Lift Operations team"]
+    ),
+   
+  ];
+  const contentHiddenStyle = {
+    backgroundColor: 'secondary.main',
+    padding: '0',
+    height: '0px',
+    transition: "height 1s ease",
+    verticalAlign: 'top',
+    border: 'none',
+    overflow: 'hidden',
+    
+   
+  };
+
+  const contentVisibleStyle = {
+    backgroundColor: 'secondary.main',
+    padding: '0',
+    height: '150px',
+    transformOrigin: 'top',
+    transition: "height 1s ease",
+    verticalAlign: 'top',
+    border: 'none',
+    overflow: 'hidden',
+  
   };
 
   return (
     <div>
       <Container
         maxWidth={false}
-        sx={{ backgroundColor: "primary.main", color: "secondary.main" }}
+        sx={{ backgroundColor: "primary.main", color: "secondary.main"}}
       >
-        <Container sx={{ paddingTop: "150px", paddingBottom: "200px" }}>
+        <Container sx={{ paddingTop: "150px", paddingBottom: "200px"}}>
           <Typography variant="h2" align="center">
             Work Experience
           </Typography>
-          {/* <Table sx= {{marginTop: '50px'}}>
-            <TableHead >
-              <TableRow >
-                <TableCell sx={tableHeadStyle1}>Company</TableCell>
-                <TableCell sx={tableHeadStyle2}>Role</TableCell>
-              </TableRow>
-            </TableHead>
+          <Table
+            sx={{
+              marginTop: "50px",
+              backgroundColor: "white",
+              
+      
+            }}
+          >
             <TableBody>
-              <TableRow>
-                <TableCell sx={tableBodyStyle1}>mStable</TableCell>
-                <TableCell sx={tableBodyStyle2}>Operations Lead</TableCell>
-              </TableRow>
-              <TableRow>
-                <TableCell sx={tableBodyStyle1}>NZSki</TableCell>
-                <TableCell sx={tableBodyStyle2}>Head of Guest Services & Reservations, Queenstown</TableCell>
-              </TableRow>
-            </TableBody>
-          </Table> */}
-          <Table sx={{ marginTop: "50px" }}>
-            <TableHead>
-              <TableRow>
-                <TableCell sx={tableHeadStyle1}>Company</TableCell>
-                <TableCell sx={tableHeadStyle2}>Role</TableCell>
-                <TableCell sx={tableHeadStyle3}>Year</TableCell>
-                <TableCell sx={tableHeadStyle4}>Description</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              <TableRow>
-                <TableCell sx={tableBodyStyle}>mStable</TableCell>
-                <TableCell sx={tableBodyStyle}>Head of Operations</TableCell>
-                <TableCell sx={tableBodyStyle}>2021-2022</TableCell>
-                <TableCell sx={tableBodyStyle}>
-                  <ul>
-                    <li>
-                      Leading operations for a team of 15 building a
-                      Decentralised Finance application
-                    </li>
-                    <li>
-                      Handling day to day HR, payroll, accounting, and legal
-                      tasks for legal entities in Germany, Australia and New
-                      Zealand as well as for a Decentralized Autonomous
-                      Organization
-                    </li>
-                    <li>
-                      Navigating a complex legal and regulatory environment in
-                      an emerging space
-                    </li>
-                    <li>
-                      Working with a global and fully remote team and managing
-                      the resulting coordination challenges
-                    </li>
-                  </ul>
-                </TableCell>
-              </TableRow>
+              {rows.map((row, index) => (
+                <>
+                  <TableRow>
+                    <TableCell component="th" scope="row">
+                      {row.year}
+                    </TableCell>
+                    <TableCell align="right">{row.company}</TableCell>
+                    <TableCell align="right">{row.title}</TableCell>
+                    <TableCell align="right">{row.location}</TableCell>
 
-              <TableRow>
-                <TableCell sx={tableBodyStyle}>NZSki</TableCell>
-                <TableCell sx={tableBodyStyle}>
-                  Guest Services Head of Department
-                </TableCell>
-                <TableCell sx={tableBodyStyle}>2020-2021</TableCell>
-                <TableCell sx={tableBodyStyle}>
-                  <ul>
-                    <li>
-                      Leading a team of frontline Guest Services staff as well
-                      as a Reservations team, responding to a demanding flow of
-                      incoming calls and emails
-                    </li>
-                    <li>
-                      Working closely with other NZSki teams to improve products
-                      and systems
-                    </li>
-                  </ul>
-                </TableCell>
-              </TableRow>
-              <TableRow>
-                <TableCell sx={tableBodyStyle}>
-                  Niseko Village Ski Area, Hokkaido, Japan
-                </TableCell>
-                <TableCell sx={tableBodyStyle}>
-                  Lift Operations Team Leader
-                </TableCell>
-                <TableCell sx={tableBodyStyle}>2016-2017</TableCell>
-                <TableCell sx={tableBodyStyle}>
-                  <ul>
-                    <li>
-                      Part of Niseko Village's first-ever group of native
-                      English-speaking Lift Operators
-                    </li>
-                  </ul>
-                </TableCell>
-              </TableRow>
-              <TableRow>
-                <TableCell sx={tableBodyStyle}>
-                  NZSki - The Remarkables Ski Area
-                </TableCell>
-                <TableCell sx={tableBodyStyle}>
-                  Lift Operations Head of Department
-                </TableCell>
-                <TableCell sx={tableBodyStyle}>2015-2019</TableCell>
-                <TableCell sx={tableBodyStyle}>
-                  <ul>
-                    <li>
-                      Managing a team of up to 45 Lift Operators and 3 Team
-                      Leaders
-                    </li>
-                    <li>
-                      Recruiting staff, organizing and coordinating training,
-                      assessing qualifications, departmental planning,
-                      developing manuals and procedures, creating rosters,
-                      purchasing, performance reviews, attending operations
-                      meetings and working closely with other departments
-                    </li>
-                    <li>
-                      Overseeing daily operations to ensure exceptional customer
-                      service
-                    </li>
-                  </ul>
-                </TableCell>
-              </TableRow>
-
-              <TableRow>
-                <TableCell sx={tableBodyStyle}>
-                  NZSki - The Remarkables Ski Area
-                </TableCell>
-                <TableCell sx={tableBodyStyle}>
-                  Lift Operations Team Leader
-                </TableCell>
-                <TableCell sx={tableBodyStyle}>2014</TableCell>
-                <TableCell sx={tableBodyStyle}>
-                  <ul>
-                    <li>Helping to lead a team of 35 Lift Operators</li>
-                  </ul>
-                </TableCell>
-              </TableRow>
-
-              <TableRow>
-                <TableCell sx={tableBodyStyle}>
-                  NZSki - The Remarkables Ski Area
-                </TableCell>
-                <TableCell sx={tableBodyStyle}>Lift Operator</TableCell>
-                <TableCell sx={tableBodyStyle}>2012-2013</TableCell>
-                <TableCell sx={tableBodyStyle}>
-                  <ul>
-                    <li>Working as part of the Lift Operations team</li>
-                  </ul>
-                </TableCell>
-              </TableRow>
+                    <TableCell
+                      align="right"
+                      onClick={() => toggleRowShown(index)}
+                    >
+                      {
+                        <i
+                          className={
+                            tableRowsShown[index]
+                              ? "fas fa-minus"
+                              : "fas fa-plus"
+                          }
+                        />
+                      }
+                    </TableCell>
+                  </TableRow>
+                
+                  <TableRow>
+                    <TableCell
+                      sx={{border: '0', padding: '0'}
+                      }
+                      align="left"
+                      colSpan={5}
+                    >
+                      
+                      <Box sx={tableRowsShown[index] ? contentVisibleStyle : contentHiddenStyle}>
+                      <Typography variant="body1" sx={{padding: '10px'}} >
+                        <ul>
+                        {row.description.map((item, index) => (
+                        <>
+                        
+                          <li>{item}</li>
+                        
+                        </>
+                        )
+                        )}
+                        </ul>
+                      </Typography>
+                      </Box>
+                     
+                    </TableCell>
+                  </TableRow>
+                </>
+              ))}
             </TableBody>
           </Table>
         </Container>
