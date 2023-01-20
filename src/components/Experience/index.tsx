efda3752412fbf12a286e9a0a8110067e117cd3c
Cam <cameron.soulsby@gmail.com>
2023-01-20 17:57:32 +1300

    update cucursor on +/- icon

--- src/components/Experience/index.tsx
+++ src/components/Experience/index.tsx
@@ -30,40 +30,41 @@ export const Experience: React.FunctionComponent = () => {
   }
   // add data
   const rows = [
-    createData(
-      "2021-2022",
-      "mStable",
-      "Head of Operations",
-      "Remote",
-      ["Leading operations for a team of 15 building a Decentralised Finance application",
-       "Handling day to day HR, payroll, accounting, and legal tasks for legal entities in Germany, Australia and New Zealand as well as for a Decentralized Autonomous Organization",
+    createData("2021-2022", "mStable", "Head of Operations", "Remote", [
+      "Leading operations for a team of 15 building a Decentralised Finance application",
+      "Handling day to day HR, payroll, accounting, and legal tasks for legal entities in Germany, Australia and New Zealand as well as for a Decentralized Autonomous Organization",
       "Navigating a complex legal and regulatory environment in an emerging space",
-      "Working with a global and fully remote team and managing the resulting coordination challenges"
-    ]
-    ),
+      "Working with a global and fully remote team and managing the resulting coordination challenges",
+    ]),
     createData(
       "2020-2021",
       "NZSki",
       "Guest Services Head of Department",
       "Queenstown, New Zealand",
-      ["Leading a team of frontline Guest Services staff as well as a Reservations team, responding to a demanding flow of incoming calls and emails",
-       "Working closely with other NZSki teams to improve products and systems "]
+      [
+        "Leading a team of frontline Guest Services staff as well as a Reservations team, responding to a demanding flow of incoming calls and emails",
+        "Working closely with other NZSki teams to improve products and systems ",
+      ]
     ),
     createData(
       "2015-2019",
       "NZSki",
       "Lift Operations Head of Department",
       "Queenstown, New Zealand",
-      ["Managing a team of up to 45 Lift Operators and 3 Team Leaders",
-       "Recruiting staff, organising and co-ordinating training, assessing qualifications, departmental planning, developing manuals and procedures, creating rosters, purchasing, performance reviews, attending operations meetings and working closely with other departments",
-        "Overseeing daily operations to ensure exceptional customer service"]
+      [
+        "Managing a team of up to 45 Lift Operators and 3 Team Leaders",
+        "Recruiting staff, organising and co-ordinating training, assessing qualifications, departmental planning, developing manuals and procedures, creating rosters, purchasing, performance reviews, attending operations meetings and working closely with other departments",
+        "Overseeing daily operations to ensure exceptional customer service",
+      ]
     ),
     createData(
       "2016-2017",
       "Niseko Village",
       "Lift Operations Team Leader",
       "Hokkaido, Japan",
-      ["Part of Niseko Village’s first-ever group of native English-speaking Lift Operators"]
+      [
+        "Part of Niseko Village’s first-ever group of native English-speaking Lift Operators",
+      ]
     ),
     createData(
       "2014",
@@ -79,39 +80,35 @@ export const Experience: React.FunctionComponent = () => {
       "Queenstown, New Zealand",
       ["Working as part of the Lift Operations team"]
     ),
-   
   ];
   const contentHiddenStyle = {
-    backgroundColor: 'secondary.main',
-    padding: '0',
-    height: '0px',
-    transition: "height 1s ease",
-    verticalAlign: 'top',
-    border: 'none',
-    overflow: 'hidden',
-    
-   
+    backgroundColor: "secondary.main",
+    padding: "0",
+    height: "0px",
+    transition: "height 0.5s ease",
+    verticalAlign: "top",
+    border: "none",
+    overflow: "hidden",
   };
 
   const contentVisibleStyle = {
-    backgroundColor: 'secondary.main',
-    padding: '0',
-    height: '150px',
-    transformOrigin: 'top',
-    transition: "height 1s ease",
-    verticalAlign: 'top',
-    border: 'none',
-    overflow: 'hidden',
-  
+    backgroundColor: "secondary.main",
+    padding: "0",
+    height: "150px",
+    transformOrigin: "top",
+    transition: "height 0.5s ease",
+    verticalAlign: "top",
+    border: "none",
+    overflow: "hidden",
   };
 
   return (
     <div>
       <Container
         maxWidth={false}
-        sx={{ backgroundColor: "primary.main", color: "secondary.main"}}
+        sx={{ backgroundColor: "primary.main", color: "secondary.main" }}
       >
-        <Container sx={{ paddingTop: "150px", paddingBottom: "200px"}}>
+        <Container sx={{ paddingTop: "150px", paddingBottom: "200px" }}>
           <Typography variant="h2" align="center">
             Work Experience
           </Typography>
@@ -119,24 +116,23 @@ export const Experience: React.FunctionComponent = () => {
             sx={{
               marginTop: "50px",
               backgroundColor: "white",
-              
-      
             }}
           >
             <TableBody>
               {rows.map((row, index) => (
                 <>
                   <TableRow>
-                    <TableCell component="th" scope="row">
+                    <TableCell >
                       {row.year}
                     </TableCell>
                     <TableCell align="right">{row.company}</TableCell>
                     <TableCell align="right">{row.title}</TableCell>
                     <TableCell align="right">{row.location}</TableCell>
-
                     <TableCell
                       align="right"
                       onClick={() => toggleRowShown(index)}
+                      sx={{cursor: 'pointer', width: '50px'}}
+                      
                     >
                       {
                         <i
@@ -149,30 +145,30 @@ export const Experience: React.FunctionComponent = () => {
                       }
                     </TableCell>
                   </TableRow>
-                
+
                   <TableRow>
                     <TableCell
-                      sx={{border: '0', padding: '0'}
-                      }
+                      sx={{ border: "0", padding: "0" }}
                       align="left"
                       colSpan={5}
                     >
-                      
-                      <Box sx={tableRowsShown[index] ? contentVisibleStyle : contentHiddenStyle}>
-                      <Typography variant="body1" sx={{padding: '10px'}} >
-                        <ul>
-                        {row.description.map((item, index) => (
-                        <>
-                        
-                          <li>{item}</li>
-                        
-                        </>
-                        )
-                        )}
-                        </ul>
-                      </Typography>
+                      <Box
+                        sx={
+                          tableRowsShown[index]
+                            ? contentVisibleStyle
+                            : contentHiddenStyle
+                        }
+                      >
+                        <Typography variant="body1" sx={{ padding: "10px" }}>
+                          <ul>
+                            {row.description.map((item, index) => (
+                              <>
+                                <li>{item}</li>
+                              </>
+                            ))}
+                          </ul>
+                        </Typography>
                       </Box>
-                     
                     </TableCell>
                   </TableRow>
                 </>
