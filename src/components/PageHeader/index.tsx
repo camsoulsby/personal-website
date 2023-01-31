274318a72b5a7f263c9df6af74ebb5c27fcffa50
Cam <cameron.soulsby@gmail.com>
2023-01-31 15:30:29 +1300

    #16 redesign header section

--- src/components/PageHeader/index.tsx
+++ src/components/PageHeader/index.tsx
@@ -1,4 +1,4 @@
-import { Typography, Container } from "@mui/material";
+import { Typography, Container, Box } from "@mui/material";
 
 type PageHeaderProps = {};
 
@@ -8,15 +8,35 @@ export const PageHeader: React.FunctionComponent<PageHeaderProps> = () => {
       <Container
         maxWidth={false}
         sx={{
+          position: "relative",
           paddingTop: "120px",
-          height: "360px",
-          // background: 'url("../images/paisley.jpg") top center/cover no-repeat',
-          // filter: 'brightness(50%)'
+          height: "600px",
+
           backgroundColor: "primary.main",
         }}
       >
+        <Box
+          sx={{
+            position: "absolute",
+            top: "0",
+            left: "0",
+            height: "100%",
+            width: "100%",
+            background:
+              'url("../images/paisley.jpg") top center/cover no-repeat',
+            filter: "brightness(40%)",
+          }}
+        ></Box>
         <Typography
-          sx={{ typography: { sm: "h2", xs: "h3" } }}
+          sx={{
+            position: "absolute",
+            top: ["220px", "230px", "210px"],
+            left: "50%",
+            marginLeft: ["-150px", "-300px", "-400px"],
+            height: "auto",
+            width: ["300px", "600px", "800px"],
+            typography: ["h3", "h2", "h1"],
+          }}
           align="center"
           color="text.secondary"
         >
@@ -24,7 +44,15 @@ export const PageHeader: React.FunctionComponent<PageHeaderProps> = () => {
         </Typography>
 
         <Typography
-          sx={{ typography: { sm: "h5", xs: "h6" }, paddingTop: "50px" }}
+          sx={{
+            position: "absolute",
+            top: ["300px", "320px", "340px"],
+            left: "50%",
+            marginLeft: ["-150px", "-300px", "-400px"],
+            height: "auto",
+            width: ["300px", "600px", "800px"],
+            typography: ["h6", "h5", "h4"],
+          }}
           color="text.secondary"
           align="center"
         >
