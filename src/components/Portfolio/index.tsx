a60dafdbdb7927f14f937db88c98c7f8774d5cde
Cam <cameron.soulsby@gmail.com>
2024-01-20 06:06:09 +1300

    add link to callreview app

--- src/components/Portfolio/index.tsx
+++ src/components/Portfolio/index.tsx
@@ -39,7 +39,7 @@ export const Portfolio: React.FunctionComponent<PortfolioProps> = () => {
                 description="AI phone call review application utilizing OpenAI APIs, built using React, Material UI, and TypeScript."
                 image="../images/callreview.png"
                 githubLink=""
-                siteLink=""
+                siteLink="https://www.callreview.camsoulsby.com"
               />
             </Grid>
             <Grid item>
--- src/components/PortfolioCard/index.tsx
+++ src/components/PortfolioCard/index.tsx
@@ -45,7 +45,9 @@ export const PortfolioCard: React.FC<PortfoloioCardProps> = ({
         <Box
           sx={{
             backgroundColor: "primary.dark",
-            background: image ? `url("${image}") top center/cover no-repeat` : "",
+            background: image
+              ? `url("${image}") top center/cover no-repeat`
+              : "",
             height: "100%",
             width: "100%",
             filter: ["none", showInfo ? "brightness(40%)" : "none"],
@@ -72,21 +74,23 @@ export const PortfolioCard: React.FC<PortfoloioCardProps> = ({
         >
           {description}
         </Typography>
-        <GitHub
-          sx={{
-            transform: ["scale(1.4)", "scale(1.8)"],
-            opacity: ["1", showInfo ? "1" : "0"],
-            position: "absolute",
-            color: ["primary.dark", "primary.contrastText"],
-            top: ["200px", "184px"],
-            left: ["35px", "20px"],
-            transition: ["none", "opacity 0.5s ease-in-out"],
-            "&:hover": {
-              color: "secondary.light",
-            },
-          }}
-          onClick={() => window.open(githubLink, "_blank")}
-        />
+        {githubLink && (
+          <GitHub
+            sx={{
+              transform: ["scale(1.4)", "scale(1.8)"],
+              opacity: ["1", showInfo ? "1" : "0"],
+              position: "absolute",
+              color: ["primary.dark", "primary.contrastText"],
+              top: ["200px", "184px"],
+              left: ["35px", "20px"],
+              transition: ["none", "opacity 0.5s ease-in-out"],
+              "&:hover": {
+                color: "secondary.light",
+              },
+            }}
+            onClick={() => window.open(githubLink, "_blank")}
+          />
+        )}
 
         <OpenInNew
           sx={{
